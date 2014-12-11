efccfeae86575555077f23a2261c476bbfe0d604
Bryan Joseph <bryanjos@gmail.com>
2014-12-10 22:18:35 -0600

    Removed mix task for seeding. Using resources now for some routes

--- web/router.ex
+++ web/router.ex
@@ -23,31 +23,18 @@ defmodule CastPub.Router do
   end
 
   post  "/api/auth",  CastPub.AuthController, :create
-  post  "/api/user", CastPub.UserController, :create
+  post  "/api/user",  CastPub.UserController, :create
 
   scope "/api" do
     pipe_through :api
 
-    get  "/user", CastPub.UserController, :show
-    patch  "/user", CastPub.UserController, :update
-    delete "/user", CastPub.UserController, :destroy
+    get     "/user",  CastPub.UserController, :show
+    patch   "/user",  CastPub.UserController, :update
+    delete  "/user",  CastPub.UserController, :destroy
 
-    get "/podcasts", CastPub.PodcastController, :index
-    post "/podcasts", CastPub.PodcastController, :create
-    get "/podcasts/:id", CastPub.PodcastController, :show
-    patch "/podcasts/:id", CastPub.PodcastController, :update
-    delete "/podcasts/:id", CastPub.PodcastController, :destroy
-
-    get "/permissions", CastPub.PodcastPermssionController, :index
-    post "/permissions", CastPub.PodcastPermssionController, :create
-    get "/permissions/:id", CastPub.PodcastPermssionController, :show
-    patch "/permissions/:id", CastPub.PodcastPermssionController, :update
-    delete "/permissions/:id", CastPub.PodcastPermssionController, :destroy
-
-    post "/episodes", CastPub.EpisodeController, :create
-    get "/episodes/:id", CastPub.EpisodeController, :show
-    patch "/episodes/:id", CastPub.EpisodeController, :update
-    delete "/episodes/:id", CastPub.EpisodeController, :destroy
+    resources "/podcasts",    CastPub.PodcastController, only: [:index, :create, :show, :update, :destroy]
+    resources "/permissions", CastPub.PodcastPermssionController, only: [:index, :create, :show, :update, :destroy]
+    resources "/episodes",    CastPub.EpisodeController, only: [:create, :show, :update, :destroy]
 
   end
 end
